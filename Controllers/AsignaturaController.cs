973d3cf9e3535d30b0ecc22ccf8a57ca87ef8d42
JuanK Ruiz <juanr@microsoft.com>
2018-10-17 12:31:06 -0500

    vistas para asignaturas

---
+++ Controllers/AsignaturaController.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using platzi_asp_net_core.Models;
+
+namespace platzi_asp_net_core.Controllers
+{
+    public class AsignaturaController : Controller
+    {
+        public IActionResult Index()
+        {
+            var asignatura = new Asignatura{
+                UniqueId = Guid.NewGuid().ToString(),
+                Nombre="Programación"
+            };
+
+            ViewBag.CosaDinamica = "La Monja";
+            ViewBag.Fecha = DateTime.Now;
+
+            return View(asignatura);
+        }
+    }
+}
--- Controllers/EscuelaController.cs
+++ Controllers/EscuelaController.cs
@@ -6,13 +6,17 @@ namespace platzi_asp_net_core.Controllers
 {
     public class EscuelaController : Controller
     {
-        public  IActionResult Index()
+        public IActionResult Index()
         {
             var escuela = new Escuela();
-            escuela.AñoDeCreación=2005;
+            escuela.AñoDeCreación = 2005;
             escuela.UniqueId = Guid.NewGuid().ToString();
-            escuela.Nombre="Platzi School";
-            
+            escuela.Nombre = "Platzi School";
+            escuela.Ciudad = "Bogota";
+            escuela.Pais = "Colombia";
+            escuela.Dirección = "Avd Siempre viva";
+            escuela.TipoEscuela = TiposEscuela.Secundaria;
+
             ViewBag.CosaDinamica = "La Monja";
 
             return View(escuela);

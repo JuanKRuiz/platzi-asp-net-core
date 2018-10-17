382f350ee71490234490688e7eeb2a1f6ffb04e4
JuanK Ruiz <juanr@microsoft.com>
2018-10-17 13:04:17 -0500

    vistas parciales

--- Controllers/AsignaturaController.cs
+++ Controllers/AsignaturaController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using platzi_asp_net_core.Models;
 
@@ -8,15 +9,36 @@ namespace platzi_asp_net_core.Controllers
     {
         public IActionResult Index()
         {
-            var asignatura = new Asignatura{
-                UniqueId = Guid.NewGuid().ToString(),
-                Nombre="Programación"
-            };
+            return View(new Asignatura{Nombre="Programación",
+                                UniqueId= Guid.NewGuid().ToString()
+                            });
+        }
+
+        public IActionResult MultiAsignatura()
+        {
+            var listaAsignaturas = new List<Asignatura>(){
+                            new Asignatura{Nombre="Matemáticas",
+                                UniqueId= Guid.NewGuid().ToString()
+                            } ,
+                            new Asignatura{Nombre="Educación Física",
+                                UniqueId= Guid.NewGuid().ToString()
+                            },
+                            new Asignatura{Nombre="Castellano",
+                                UniqueId= Guid.NewGuid().ToString()
+                            },
+                            new Asignatura{Nombre="Ciencias Naturales",
+                                UniqueId= Guid.NewGuid().ToString()
+                            }
+                            ,
+                            new Asignatura{Nombre="Programación",
+                                UniqueId= Guid.NewGuid().ToString()
+                            }
+                };
 
             ViewBag.CosaDinamica = "La Monja";
             ViewBag.Fecha = DateTime.Now;
 
-            return View(asignatura);
+            return View("MultiAsignatura", listaAsignaturas);
         }
     }
 }

a5c116cee2cf870378db227a86f6cf1d86daa022
JuanK Ruiz <juanr@microsoft.com>
2018-10-17 10:35:29 -0500

    App created

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace platzi_asp_net_core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

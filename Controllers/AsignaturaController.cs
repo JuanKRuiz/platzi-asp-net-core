f7f8b712f7291ee9534ad6be18cda61b0275e6a0
JuanK Ruiz <juanr@microsoft.com>
2018-10-18 11:36:32 -0500

    ajustes ruta asignaturas

--- Controllers/AsignaturaController.cs
+++ Controllers/AsignaturaController.cs
@@ -8,9 +8,22 @@ namespace platzi_asp_net_core.Controllers
 {
     public class AsignaturaController : Controller
     {
-        public IActionResult Index()
+        [Route("Asignatura/Index")]
+        [Route("Asignatura/Index/{asignaturaId}")]
+        public IActionResult Index(string asignaturaId)
         {
-            return View(_context.Asignaturas.FirstOrDefault() );
+            if(!string.IsNullOrWhiteSpace(asignaturaId))
+            {
+                        var asignatura = from asig in _context.Asignaturas
+                                        where asig.Id == asignaturaId
+                                        select asig;
+
+                        return View(asignatura.SingleOrDefault());
+            }
+            else
+            {
+               return View("MultiAsignatura", _context.Asignaturas); 
+            }
         }
 
         public IActionResult MultiAsignatura()

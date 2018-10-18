b3240b8de29cd88be4df4efe6aef7d44b22115a0
JuanK Ruiz <juanr@microsoft.com>
2018-10-18 15:18:55 -0500

    ejemplos de data annotations

--- Models/Curso.cs
+++ Models/Curso.cs
@@ -6,12 +6,16 @@ namespace platzi_asp_net_core.Models
 {
     public class Curso:ObjetoEscuelaBase
     {
-        [Required]
+        [Required(ErrorMessage= "El nombre del curso es requerido")]
+        [StringLength(5)]
         public override string Nombre {set;get;}
         public TiposJornada Jornada { get; set; }
         public List<Asignatura> Asignaturas{ get; set; }
         public List<Alumno> Alumnos{ get; set; }
 
+        [Display(Prompt="Dirección correspondencia", Name="Address")]
+        [Required(ErrorMessage= "Se requiere incluir una dirección")]
+        [MinLength(10, ErrorMessage="La longitud mínima de la dirección es 10")]
         public string Dirección { get; set; }
 
         public string EscuelaId { get; set; }
--- Models/EscuelaContext.cs
+++ Models/EscuelaContext.cs
@@ -90,11 +90,16 @@ namespace platzi_asp_net_core.Models
                             Id = Guid.NewGuid().ToString(),
                             EscuelaId = escuela.Id,
                             Nombre = "101",
-                            Jornada = TiposJornada.Mañana },
-                        new Curso() {Id = Guid.NewGuid().ToString(), EscuelaId = escuela.Id, Nombre = "201", Jornada = TiposJornada.Mañana},
-                        new Curso   {Id = Guid.NewGuid().ToString(), EscuelaId = escuela.Id, Nombre = "301", Jornada = TiposJornada.Mañana},
-                        new Curso() {Id = Guid.NewGuid().ToString(), EscuelaId = escuela.Id, Nombre = "401", Jornada = TiposJornada.Tarde },
-                        new Curso() {Id = Guid.NewGuid().ToString(), EscuelaId = escuela.Id, Nombre = "501", Jornada = TiposJornada.Tarde},
+                            Jornada = TiposJornada.Mañana,
+                            Dirección="Avenida siempre viva"
+                             },
+                        new Curso() {Id = Guid.NewGuid().ToString(),
+                         EscuelaId = escuela.Id, Nombre = "201", 
+                         Dirección="Avenida siempre viva",
+                         Jornada = TiposJornada.Mañana},
+                        new Curso   {Id = Guid.NewGuid().ToString(), EscuelaId = escuela.Id, Nombre = "301",Dirección="Avenida siempre viva", Jornada = TiposJornada.Mañana},
+                        new Curso() {Id = Guid.NewGuid().ToString(), EscuelaId = escuela.Id, Nombre = "401", Dirección="Avenida siempre viva",Jornada = TiposJornada.Tarde },
+                        new Curso() {Id = Guid.NewGuid().ToString(), EscuelaId = escuela.Id, Nombre = "501", Dirección="Avenida siempre viva",Jornada = TiposJornada.Tarde},
             };
         }
 
